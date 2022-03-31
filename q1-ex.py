ba343d6c29643ada37bf4c59df07900de900c640
teenaannajohn <74650841+teenaannajohn@users.noreply.github.com>
2022-03-31 11:05:49 +0530

    Add files via upload

---
+++ q1-ex.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a=np.array([[2,4],[6,2]])
+b=np.array([[5,7],[4,8]])
+#print(a)
+print("Transpose of the matrix a:\n",a.transpose())
+print("Transpose of the matrix b:\n",b.transpose())
+print("sum of the matrices:\n",np.add(a,b))
